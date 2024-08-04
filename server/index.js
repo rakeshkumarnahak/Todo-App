2c2f426d1182012ade8f967a92ba1c6a0f869999
rakeshkumarnahak <rakeshkumarnahak1@gmail.com>
2024-08-04 21:18:50 +0530

    App is connected to database

--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("App connected to Database");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 app.get("/", (req, res) => {
   res.status(500).json({ message: "All good, server is saying Hello World!" });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
