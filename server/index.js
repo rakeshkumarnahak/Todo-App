d7d7f798b16c7f77e141580ad97c2cd168a31eb0
Subrajeet Maharana <subrajeet.info@gmail.com>
2024-08-05 21:05:20 +0530

    minor changes in index.js

--- server/index.js
+++ server/index.js
@@ -22,3 +22,14 @@ mongoose
 app.get("/", (req, res) => {
   res.status(500).json({ message: "All good, server is saying Hello World!" });
 });
+
+app.post("signup", async (req, res) => {
+  try {
+    const newUser = { ...req.body, id: Date.now() };
+    console.log(newUser);
+    const user = await User.create();
+    res.status(200).json({ message: "User created Successfully", user: user });
+  } catch (error) {
+    console.log(error);
+  }
+});
