ea6be24a404bb5f23401cedb9676deaf833b136c
Subrajeet Maharana <subrajeet.info@gmail.com>
2024-08-08 20:31:21 +0530

    added signin and sign up components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./components/Signup.jsx";
+import Login from "./components/Login.jsx";
 
 export default function App() {
   return (
-    <h1 className="text-3xl bg-violet-600 font-bold underline">Hello world!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
