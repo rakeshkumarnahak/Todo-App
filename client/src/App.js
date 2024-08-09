cb918c09031189d3adb481b7578a93056ef6ae54
Subrajeet Maharana <subrajeet.info@gmail.com>
2024-08-09 19:42:33 +0530

    added TodoCard, TodoList components

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./components/Signup.jsx";
 import Login from "./components/Login.jsx";
+import TodoList from "./components/TodoList";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login" element={<Login />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/todo" element={<TodoList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
